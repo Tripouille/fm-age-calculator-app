7fbd22f9611e458611bfdb9d3fa83ad09528229c
Jean-Michel <jmichel.gambard@gmail.com>
2023-04-02 09:47:36 +0200

    feat: setup poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,14 @@
+import { Poppins } from "next/font/google";
+
 export const metadata = {
   title: "Age Calculator App",
   description: "Frontend Mentor Challenge",
 };
+
+const poppins = Poppins({
+  weight: ["400", "700", "800"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>{children}</body>
     </html>
   );
